67b013ba744a6a82c937c44c1156c3a059acc715
hwalborn <hwalborn@gmail.com>
2017-06-20 14:54:12 -0400

    recursive check

--- app/functions.js
+++ app/functions.js
@@ -2,31 +2,38 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+    return fn(arr[0], arr[1]);
   },
 
   speak: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   functionFunction: function(str) {
-
+    const greeting = (str2) => {
+      return `${str}, ${str2}`
+    }
+    return greeting
   },
 
   makeClosures: function(arr, fn) {
-
+    return arr.map((num) =>  {
+      return fn.bind(null, num)
+    })
   },
 
   partial: function(fn, str1, str2) {
-
+    return fn.bind(null, str1, str2)
   },
 
   useArguments: function() {
-
+    return Array.prototype.reduce.call(arguments, (acc, num) => {
+      return typeof num === "number" ? acc += num : acc
+    }, 0)
   },
 
   callIt: function(fn) {
-
+    // let argArray = 
   },
 
   partialUsingArguments: function(fn) {
--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,38 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-    
+
+    const foundFiles = [];
+
+    const findWithDirectory = (data, subDir) => {
+      // Until there's the right subdirectory, go one step deeper
+      if(!!subDir && data.dir !== subDir){
+          data.files.forEach((file) => {
+            if (typeof file === 'object'){
+              findWithDirectory(file, subDir)
+          }
+        })
+      } else {
+        func(data)
+      }
+    }
+
+    const func = (data) => {
+      data.files.forEach((file) => {
+        if(typeof file === 'string') {
+          foundFiles.push(file)
+        } else {
+          func(file)
+        }
+      })
+    }
+
+    if(!!dirName) {
+      findWithDirectory(data, dirName)
+    } else {
+      func(data)
+    }
+    return foundFiles
   },
 
   permute: function(arr) {
