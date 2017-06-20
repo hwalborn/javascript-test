fbc468a9797ad2c9c5574b4a6bf402d54e1e32bb
hwalborn <hwalborn@gmail.com>
2017-06-20 18:41:19 -0400

    objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj)
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting
   },
 
   iterate: function(obj) {
-
+    let iterate = []
+    for(key in obj) {
+      debugger
+      iterate.push(`${key}: ${obj[key]}`)
+    }
+    return iterate
   }
 };
