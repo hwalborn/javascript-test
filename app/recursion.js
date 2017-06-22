08b83b9c17cdc9c4ca26b8a5637d1e5a7b9bb698
hwalborn <hwalborn@gmail.com>
2017-06-22 08:55:07 -0400

    refactored a bit

--- app/recursion.js
+++ app/recursion.js
@@ -8,9 +8,9 @@ exports.recursionAnswers = {
     const findWithDirectory = (data, subDir) => {
       // Until there's the right subdirectory, go one step deeper
       if(!!subDir && data.dir !== subDir){
-          data.files.forEach((file) => {
-            if (typeof file === 'object'){
-              findWithDirectory(file, subDir)
+        data.files.forEach((file) => {
+          if (typeof file === 'object'){
+            findWithDirectory(file, subDir)
           }
         })
       } else {
@@ -26,29 +26,6 @@ exports.recursionAnswers = {
           findFiles(file)
         }
       })
-    // function recur(data, seeking=false, arr=[]) {
-    //   if (Array.isArray(data)) {
-    //     return data.forEach(item => recur(item, seeking, arr))
-    //   } else if (typeof data === 'object') {
-    //     if (data.dir === seeking)
-    //       seeking = false
-    //     return recur(data.files, seeking, arr) || arr
-    //   } else if (!seeking) {
-    //     arr.push(data)
-    //   }
-    // }
-
-    // function recurAnno(data, seeking=false, arr=[]) {
-    //   if (Array.isArray(data)) { // if we are examining a directory's items
-    //     data.forEach(item => recur(item, seeking, arr)) // examine each item
-    //   } else if (typeof data === 'object') { // if we are examining a directory's meta data
-    //     if (data.dir === seeking) // if these are the droids we are looking for...
-    //       seeking = false // ...turn on our 'capture all files' mode
-    //     return recur(data.files, seeking, arr) || arr // this || is because our recursion on files returns undefined. could always return arr.concat(data) on our file recursion, but that is sloppy dop expensive
-    //   } else if (!seeking) { // otherwise we are examining a file (string)
-    //     arr.push(data) // add it to our arr if we are capturing
-    //   }
-    // }
     }
 
     if(!!dirName) {
@@ -66,6 +43,7 @@ exports.recursionAnswers = {
 
   fibonacci: function(n) {
     let fibs = []
+
     const fib = (num, prev) => {
       fibs.push(num)
       if(fibs.indexOf(num) === n - 1) {
@@ -74,7 +52,9 @@ exports.recursionAnswers = {
         fib(num + prev, num)
       }
     }
+
     fib(0, 1)
+
     return fibs[fibs.length - 1]
   },
 
@@ -89,22 +69,26 @@ exports.recursionAnswers = {
     let i = 0
     const vals = []
 
+    const incrementAndCall = () => {
+      i++
+      validateParens(arr[i])
+    }
+
     const validateParens = (keyOrValue) => {
       let includedInKeys = Object.keys(parens).includes(keyOrValue)
       if((!includedInKeys && vals.length === 0) || (Object.values(parens).includes(keyOrValue) && keyOrValue !== vals[vals.length - 1])) {
         return
       } else if(includedInKeys){
-        i++
         vals.push(parens[keyOrValue])
-        validateParens(arr[i])
+        incrementAndCall()
       } else {
-        i++
         vals.pop()
-        validateParens(arr[i])
+        incrementAndCall()
       }
     }
 
     validateParens(arr[i])
+    
     return vals.length === 0
   }
 };
